105a1736e72f0a392c9b547a97e7db4d75e970a7
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-10-19 18:15:11 +0200

    Update main.go

--- main.go
+++ main.go
@@ -16,9 +16,8 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title: "Ark Ascended Server Manager",
-		Width: 1024,
-
+		Title:  "Ark Ascended Server Manager",
+		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
