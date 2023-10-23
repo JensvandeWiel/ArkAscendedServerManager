c0d4f83e8bd0181591662de0e403ae4726560432
August2211 <53059031+August2211@users.noreply.github.com>
2023-10-23 13:00:45 +0200

    Returning the error directly

--- helpers/discord.go
+++ helpers/discord.go
@@ -2,15 +2,13 @@ package helpers
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
-
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // SendToDiscord sends the given message to the webhook url
-func SendToDiscord(message string, webhookUrl string) bool {
+func SendToDiscord(message string, webhookUrl string) error {
 
 	body := map[string]string{
 		"content": message,
@@ -19,21 +17,18 @@ func SendToDiscord(message string, webhookUrl string) bool {
 	jsonBody, err := json.Marshal(body)
 
 	if err != nil {
-		runtime.LogError(context.Background(), "Error marshalling body json: "+err.Error())
-		return false
+		return fmt.Errorf("error marshalling body json: %s", err.Error())
 	}
 
 	response, err := http.Post(webhookUrl, "application/json", bytes.NewBuffer(jsonBody))
 
 	if err != nil {
-		runtime.LogError(context.Background(), "Error sending message to discord: "+err.Error())
-		return false
+		return fmt.Errorf("error sending message to discord: %s", err.Error())
 	}
 
 	if response.StatusCode != 204 {
-		runtime.LogError(context.Background(), "Error sending message to discord: "+response.Status)
-		return false
+		return fmt.Errorf("error sending message to discord: %s", response.Status)
 	}
 
-	return true
+	return nil
 }
