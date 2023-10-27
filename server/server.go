ff35e235554179a1de198b6de4d55f9ecb7eeab4
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-10-27 14:02:53 +0200

    added serverPath

--- server/server.go
+++ server/server.go
@@ -2,8 +2,14 @@ package server
 
 // Server contains the server "stuff"
 type Server struct {
-	Id          int    `json:"id"`
+	// Id is the id of the server
+	Id int `json:"id"`
+
+	// ServerAlias is the name of the config (an alias for the server)
 	ServerAlias string `json:"serverAlias"`
+
+	// ServerPath is the path where the server is installed/should be.
+	ServerPath string `json:"serverPath"`
 
 	//Server Name and Passwords
 	ServerName string `json:"serverName"`
