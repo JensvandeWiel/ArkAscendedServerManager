0cf4863cb531c65732d80583a99c02bceceae9a2
ItsMePepijn <68758035+ItsMePepijn@users.noreply.github.com>
2023-11-01 17:38:53 +0100

    hopefully fixed pipeline for the last time

--- build/updateConfig.js
+++ build/updateConfig.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
-const fileName = '../wails.json';
-const file = require(fileName);
+const fileName = 'wails.json';
+const file = require("../" + fileName);
     
 console.log("changing version to " + process.argv[2] + ", isNightly to " + process.argv[3] + " and environment to prod")
 
-file.version = process.argv[2].replace("v", "");
-file.info.version = file.version;
+const versionNum = `${process.argv[2]}`.replace("v", "")
+file.version = versionNum;
+file.info.productVersion = versionNum;
 file.isNightly = process.argv[3] === 'true';
 file.environment = "prod"
+
+if(process.argv[2] === undefined || process.argv[2] === "" || process.argv[3] === undefined || process.argv[3] === "") {
+  throw new Error("version and isNightly must be set")
+}
     
 fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
   if (err) return console.log(err);
-  console.log(JSON.stringify(file));
-  console.log('Wrote to ' + fileName + "!");
+  console.log(JSON.stringify(file, null, 2));
 });
