61ace9d80c8d9c78d04ae9236188f39e823b1249
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-11-01 14:45:11 +0100

    added basic console layout

--- frontend/src/pages/server/Console.tsx
+++ frontend/src/pages/server/Console.tsx
@@ -1,6 +1,9 @@
-import {TabPanel} from "@mui/joy";
-import React from "react";
+import {TabPanel, Typography} from "@mui/joy";
+import React, {useEffect, useState} from "react";
 import {server} from "../../../wailsjs/go/models";
+import {GetServerStatus} from "../../../wailsjs/go/server/ServerController";
+import {useAlert} from "../../components/AlertProvider";
+import {EventsOn} from "../../../wailsjs/runtime";
 
 type Props = {
     setServ: React.Dispatch<React.SetStateAction<server.Server>>
@@ -9,9 +12,44 @@ type Props = {
 }
 
 export function Console({setServ, serv}:Props) {
-    return (
-        <TabPanel value={0} className={''}>
-            sdssd
-        </TabPanel>
-    );
+
+    const [serverStarted, setServerStarted] = useState(false)
+    const {addAlert} = useAlert()
+
+    // useEffect for checking if the server is running
+    useEffect(() => {
+        GetServerStatus(serv.id).then((status) => {setServerStarted(status)}).catch((err) => {console.error(err); addAlert(err, "danger")})
+    }, []);
+
+    // Register server status events
+    useEffect(() => {
+        EventsOn("onServerStart", (id: number) => {
+            if (id === serv.id) {
+                setServerStarted(true)
+            }
+        })
+        EventsOn("onServerExit", (id: number) => {
+            if (id === serv.id) {
+                setServerStarted(false)
+            }
+        })
+    }, []);
+
+    if (serverStarted) {
+        return (
+            <TabPanel value={0}>
+                server started
+            </TabPanel>
+        );
+    } else {
+        return (
+            <TabPanel value={0}>
+                <div className={'h-16 flex w-full'}>
+                    <p className={'text-lg font-bold ml-8'}>Server is not running</p>
+                </div>
+
+            </TabPanel>
+        );
+    }
+
 }
