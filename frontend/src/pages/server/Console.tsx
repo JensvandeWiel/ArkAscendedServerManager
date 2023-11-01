be21008dc2631a8ff4ac81586d3f8206ad54af0c
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-11-01 17:05:39 +0100

    added basic terminal

--- frontend/src/pages/server/Console.tsx
+++ frontend/src/pages/server/Console.tsx
@@ -1,9 +1,8 @@
-import {TabPanel, Typography} from "@mui/joy";
-import React, {useEffect, useState} from "react";
+import {Button, Input, TabPanel, Typography} from "@mui/joy";
+import React, {useEffect, useRef, useState} from "react";
 import {server} from "../../../wailsjs/go/models";
-import {GetServerStatus} from "../../../wailsjs/go/server/ServerController";
 import {useAlert} from "../../components/AlertProvider";
-import {EventsOn} from "../../../wailsjs/runtime";
+import {IconArrowRight, IconChevronRight} from "@tabler/icons-react";
 
 type Props = {
     setServ: React.Dispatch<React.SetStateAction<server.Server>>
@@ -14,13 +13,33 @@ type Props = {
 
 export function Console({setServ, serv, serverStatus}:Props) {
 
-    const {addAlert} = useAlert()
+    const { addAlert  } = useAlert()
 
+    const [input, setInput] = useState("")
+    const terminalRef = useRef<HTMLDivElement>(null);
 
-    if (serverStatus) {
+    function writeToConsole(text: string, sender: string = "") {
+        if (terminalRef.current) {
+
+            terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+            terminalRef.current.innerHTML += "<span>  " + sender + "&nbsp;>&nbsp;"+ text + " <br/></span>"
+        }
+    }
+
+    function handleInput() {
+
+    }
+
+    if (!serverStatus) {
         return (
             <TabPanel value={0}>
-                server started
+                <div id={"terminal"} ref={terminalRef} className={"overflow-y-scroll font-bold bg-black w-[100%] h-[50vh] p-4 rounded "}></div>
+                <Input className={"my-2 font-jetbrains font-bold"} value={input} onChange={(e) => setInput(e.target.value)} startDecorator={<IconChevronRight/>} endDecorator={<Button color={"neutral"} onClick={(e) => {writeToConsole(input); setInput("")}} className={"m-1"} >Send</Button>} onKeyPress={(e) => {
+                    if (e.key === 'Enter') {
+                        writeToConsole(input)
+                        setInput("")
+                    }
+                }}></Input>
             </TabPanel>
         );
     } else {
