b725f332be22732866ac89dbab471d202d9109a6
Jens van de Wiel <jens.vdwiel@gmail.com>
2023-11-12 12:31:38 +0100

    refactored general

--- frontend/src/pages/server/General.tsx
+++ frontend/src/pages/server/General.tsx
@@ -20,8 +20,35 @@ type Props = {
 
 }
 
+function ServerNameAndPasswordsCard({ setServ, serv }: Props) {
+    return (
+        <Card variant="soft"  className={''}>
+            <Typography level="title-md">
+                Server Name and Passwords
+            </Typography>
+            <Divider className={'mx-2'}/>
 
-export function General({serv, setServ}: Props) {
+            <FormLabel>Server Name:</FormLabel>
+            <Input className={'w-2/3'} required value={serv?.serverName} onChange={(e) => setServ((p) => ({ ...p, serverName: e.target.value, convertValues: p.convertValues }))}  ></Input>
+            <div className={'space-x-4 w-full flex'}>
+                <div className={'inline-block'}>
+                    <FormLabel>Server Password:</FormLabel>
+                    <PasswordInput value={serv?.serverPassword} disabled={true} onChange={(e) => setServ((p) => ({ ...p, serverPassword: e.target.value, convertValues: p.convertValues }))} ></PasswordInput>
+                </div>
+                <div className={'inline-block'}>
+                    <FormLabel>Admin Password:</FormLabel>
+                    <PasswordInput value={serv?.adminPassword} onChange={(e) => setServ((p) => ({ ...p, adminPassword: e.target.value, convertValues: p.convertValues }))} ></PasswordInput>
+                </div>
+                <div className={'inline-block'}>
+                    <FormLabel>Spectator Password:</FormLabel>
+                    <PasswordInput value={serv?.spectatorPassword} disabled={true} onChange={(e) => setServ((p) => ({ ...p, spectatorPassword: e.target.value, convertValues: p.convertValues }))} ></PasswordInput>
+                </div>
+            </div>
+        </Card>
+    )
+}
+function NetworkingCard({ setServ, serv }: Props) {
+
     const defInter: {[p: string]: string} = {
         "none": ""
     }
@@ -42,81 +69,59 @@ export function General({serv, setServ}: Props) {
     }, []);
 
     return (
+        <Card variant="soft"  className={''}>
+            <Typography level="title-md">
+                Networking
+            </Typography>
+            <Divider className={'mx-2'}/>
+            <FormLabel>IP Address:</FormLabel>
+            <Select
+                value={serv.ipAddress?? "0.0.0.0"}
+                onChange={(e, value) => {
+                    const newValue = value;
+                    setServ(
+                        prevState =>{
+                            return{
+                                ...prevState,
+                                ipAddress: newValue?? "0.0.0.0",
+                                convertValues: prevState.convertValues
+                            }
+                        }
+                    )
+                }}
+            >
+                <Option value={"0.0.0.0"}>0.0.0.0 - All</Option>
+                {interfaceElements}
+            </Select>
+            <FormLabel>Ports: </FormLabel>
+            <div className={'space-x-4 w-11/12'}>
+                <div className={'w-32 inline-block'}>
+                    <FormLabel>Server Port:</FormLabel>
+                    <Input className={''} type={'number'} required value={serv?.serverPort} onChange={(e) => (parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 65535) ? setServ((p) => ({ ...p, serverPort: parseInt(e.target.value), peerPort: parseInt(e.target.value) + 1, convertValues: p.convertValues })) : null} ></Input>
+                </div>
+                <div className={'w-32 inline-block'}>
+                    <FormLabel>Peer Port:</FormLabel>
+                    <Input className={''} disabled type={'number'} value={serv.peerPort} ></Input>
+                </div>
+                <div className={'w-32 inline-block'}>
+                    <FormLabel>Query Port:</FormLabel>
+                    <Input className={''} type={'number'} value={serv?.queryPort} onChange={(e) => (parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 65535) ? setServ((p) => ({ ...p, queryPort: parseInt(e.target.value), convertValues: p.convertValues})) : null}></Input>
+                </div>
+                <div className={'w-32 inline-block'}>
+                    <FormLabel>RCON Port:</FormLabel>
+                    <Input className={''} value={serv?.rconPort} onChange={(e) => (parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 65535) ? setServ((p) => ({ ...p, rconPort: parseInt(e.target.value), convertValues: p.convertValues})) : null} type={'number'}></Input >
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+
+export function General({serv, setServ}: Props) {
+    return (
         <TabPanel value={1} className={'space-y-8'}>
-            {/* Server Name and Passwords */}
-            <Card variant="soft"  className={''}>
-                <Typography level="title-md">
-                    Server Name and Passwords
-                </Typography>
-                <Divider className={'mx-2'}/>
-
-{/*
-                <Portal container={document.getElementById("alert-root")}><Alert className={"fixed"}>dsadasd</Alert></Portal>
-*/}
-
-                <FormLabel>Server Name:</FormLabel>
-                <Input className={'w-2/3'} required value={serv?.serverName} onChange={(e) => setServ((p) => ({ ...p, serverName: e.target.value, convertValues: p.convertValues }))}  ></Input>
-                <div className={'space-x-4 w-full flex'}>
-                    <div className={'inline-block'}>
-                        <FormLabel>Server Password:</FormLabel>
-                        <PasswordInput value={serv?.serverPassword} disabled={true} onChange={(e) => setServ((p) => ({ ...p, serverPassword: e.target.value, convertValues: p.convertValues }))} ></PasswordInput>
-                    </div>
-                    <div className={'inline-block'}>
-                        <FormLabel>Admin Password:</FormLabel>
-                        <PasswordInput value={serv?.adminPassword} onChange={(e) => setServ((p) => ({ ...p, adminPassword: e.target.value, convertValues: p.convertValues }))} ></PasswordInput>
-                    </div>
-                    <div className={'inline-block'}>
-                        <FormLabel>Spectator Password:</FormLabel>
-                        <PasswordInput value={serv?.spectatorPassword} disabled={true} onChange={(e) => setServ((p) => ({ ...p, spectatorPassword: e.target.value, convertValues: p.convertValues }))} ></PasswordInput>
-                    </div>
-                </div>
-            </Card>
-
-            {/* Server Name and Passwords */}
-            <Card variant="soft"  className={''}>
-                <Typography level="title-md">
-                    Networking
-                </Typography>
-                <Divider className={'mx-2'}/>
-                <FormLabel>IP Address:</FormLabel>
-                <Select
-                        value={serv.ipAddress?? "0.0.0.0"}
-                        onChange={(e, value) => {
-                            const newValue = value;
-                            setServ(
-                                prevState =>{
-                                    return{
-                                        ...prevState,
-                                        ipAddress: newValue?? "0.0.0.0",
-                                        convertValues: prevState.convertValues
-                                    }
-                                }
-                            )
-                        }}
-                >
-                    <Option value={"0.0.0.0"}>0.0.0.0 - All</Option>
-                    {interfaceElements}
-                </Select>
-                <FormLabel>Ports: </FormLabel>
-                <div className={'space-x-4 w-11/12'}>
-                    <div className={'w-32 inline-block'}>
-                        <FormLabel>Server Port:</FormLabel>
-                        <Input className={''} type={'number'} required value={serv?.serverPort} onChange={(e) => (parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 65535) ? setServ((p) => ({ ...p, serverPort: parseInt(e.target.value), peerPort: parseInt(e.target.value) + 1, convertValues: p.convertValues })) : null} ></Input>
-                    </div>
-                    <div className={'w-32 inline-block'}>
-                        <FormLabel>Peer Port:</FormLabel>
-                        <Input className={''} disabled type={'number'} value={serv.peerPort} ></Input>
-                    </div>
-                    <div className={'w-32 inline-block'}>
-                        <FormLabel>Query Port:</FormLabel>
-                        <Input className={''} type={'number'} value={serv?.queryPort} onChange={(e) => (parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 65535) ? setServ((p) => ({ ...p, queryPort: parseInt(e.target.value), convertValues: p.convertValues})) : null}></Input>
-                    </div>
-                    <div className={'w-32 inline-block'}>
-                        <FormLabel>RCON Port:</FormLabel>
-                        <Input className={''} value={serv?.rconPort} onChange={(e) => (parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 65535) ? setServ((p) => ({ ...p, rconPort: parseInt(e.target.value), convertValues: p.convertValues})) : null} type={'number'}></Input >
-                    </div>
-                </div>
-            </Card>
+            <ServerNameAndPasswordsCard setServ={setServ} serv={serv} />
+            <NetworkingCard setServ={setServ} serv={serv} />
         </TabPanel>
     );
 }
