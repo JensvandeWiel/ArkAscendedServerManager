7816533d8c1006743af04f709774a35a7621608a
Jens van de Wiel <jens.vdwiel@gmail.com>
2025-06-01 19:59:23 +0200

    Cleaned up server screen

--- ui/src/main/kotlin/server/ServerScreen.kt
+++ ui/src/main/kotlin/server/ServerScreen.kt
@@ -3,7 +3,6 @@ package ui.server
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,7 +11,7 @@ import com.konyaco.fluent.component.AccentButton
 import com.konyaco.fluent.component.ListItemSeparator
 import com.konyaco.fluent.component.Text
 import com.konyaco.fluent.component.TextField
-import kotlinx.coroutines.launch
+import components.CollapsibleCard
 
 @Composable
 fun ServerScreen(component: ServerComponent) {
@@ -20,36 +19,39 @@ fun ServerScreen(component: ServerComponent) {
     val profileConfigurationModel by component.profileConfigurationModel.subscribeAsState()
     val server by component.server.subscribeAsState()
     Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(server.profileName, style = com.konyaco.fluent.FluentTheme.typography.title)
-            Spacer(modifier = Modifier.weight(1f))
-            AccentButton({
-                component.saveProfileConfiguration()
-            }, modifier = Modifier.padding(start = 8.dp)) {
-                Text("Save")
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(16.dp, 0.dp)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("Profile: " + server.profileName, style = com.konyaco.fluent.FluentTheme.typography.body)
+                Spacer(modifier = Modifier.weight(1f))
+                AccentButton({
+                    component.saveProfileConfiguration()
+                }, modifier = Modifier.padding(start = 8.dp)) {
+                    Text("Save")
+                }
             }
         }
         ListItemSeparator(Modifier)
-        Text("Profile Configuration", style = com.konyaco.fluent.FluentTheme.typography.subtitle)
-        TextField(
-            value = profileConfigurationModel.profileName,
-            onValueChange = { newValue ->
-                component.updateProfileConfigurationModel(
-                    component.profileConfigurationModel.value.copy(profileName = newValue)
-                )
-            },
-            modifier = Modifier.fillMaxWidth(),
-            header = { Text("Profile Name") },
-        )
-        TextField(
-            value = profileConfigurationModel.installationLocation,
-            onValueChange = { newValue ->
-                component.updateProfileConfigurationModel(
-                    component.profileConfigurationModel.value.copy(installationLocation = newValue)
-                )
-            },
-            modifier = Modifier.fillMaxWidth(),
-            header = { Text("Installation Location") },
-        )
+        CollapsibleCard(title = { Text("Profile") }) {
+            TextField(
+                value = profileConfigurationModel.profileName,
+                onValueChange = { newValue ->
+                    component.updateProfileConfigurationModel(
+                        component.profileConfigurationModel.value.copy(profileName = newValue)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                header = { Text("Profile Name") },
+            )
+            TextField(
+                value = profileConfigurationModel.installationLocation,
+                onValueChange = { newValue ->
+                    component.updateProfileConfigurationModel(
+                        component.profileConfigurationModel.value.copy(installationLocation = newValue)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                header = { Text("Installation Location") },
+            )
+        }
     }
 }
