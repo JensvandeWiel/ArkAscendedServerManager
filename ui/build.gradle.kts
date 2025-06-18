2a9003305b5a9adf6cc4885e6da277ac783dde03
Jens van de Wiel <jens.vdwiel@gmail.com>
2025-06-18 17:06:57 +0200

    chore: Update compose version

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -14,10 +14,6 @@ repositories {
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
     implementation(project(":core"))
     implementation(libs.decompose)
